19316cf518d5be48fa94eb19c2dd96a2ecdb9726
Abdurahman Mamu <abdimahmudhaji227@gmail.com>
2021-12-14 04:25:29 -0800

    updated

--- 0x09-python-everything_is_object/101-locked-class.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Locked class module"""
-
-
-class LockedClass:
-    """Locked class using slots to save memory"""
-    __slots__ = "first_name",
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Locked class Module """
+
+class LockedClass:
+    """Locked class using slots to save memory cost"""
+    __slots__ = "first_name",
